f361d1953a095328277eb2224f97eb7cbe107cf4
Luke Falvey <lukebfalvey@gmail.com>
2025-03-19 19:54:41 +1300

    Add lambda handler

--- src/bookings.py
+++ src/bookings.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+from email.mime.text import MIMEText
+import smtplib
 import zoneinfo
 import argparse
 import os
@@ -11,6 +13,14 @@ from client.wnba import HelloClubClient, authenticate
 
 WNBA_ACITIVTY_ID = "5aadd66e87c6b800048a2908"
 PACIFIC_AUCKLAND = zoneinfo.ZoneInfo("Pacific/Auckland")
+
+HELLO_CLUB_USERNAME = os.environ["HELLO_CLUB_USERNAME"]
+HELLO_CLUB_PASSWORD = os.environ["HELLO_CLUB_PASSWORD"]
+
+# Email
+SMTP_PASSWORD = os.environ["SMTP_PASSWORD"]
+FROM_ADDRESS = "lukebfalvey@gmail.com"
+TO_ADDRESSES = "lukebfalvey@gmail.com"  # Comma-Separated
 
 
 @dataclasses.dataclass
@@ -129,7 +139,57 @@ def print_bookings(client: HelloClubClient, from_date: datetime, to_date: dateti
         )
 
 
+def email_bookings(client: HelloClubClient, from_date: datetime, to_date: datetime):
+    bookings = client.get_bookings(from_date, to_date)
+    removed_bookings = client.get_bookings(from_date, to_date, is_removed=True)
+
+    bookings = [Booking.from_dict(booking) for booking in bookings if booking]
+    removed_bookings = [
+        Booking.from_dict(booking) for booking in removed_bookings if booking
+    ]
+
+    email_lines = []
+
+    for booking in sorted(bookings + removed_bookings):
+        email_confirmation = (
+            booking.confirmation_email_sent.strftime("%Y-%m-%d %H:%M:%S")
+            if booking.confirmation_email_sent
+            else ""
+        )
+        start_time = booking.start_time.strftime("%H:%M:%S")
+        end_time = booking.end_time.strftime("%H:%M:%S")
+        if booking.removed_on:
+            removed_on = booking.removed_on.strftime("%Y-%m-%d %H:%M:%S")
+            removed_by = booking.removed_by if booking.removed_by else "UNKNOWN"
+            removed_message = f"(REMOVED {removed_on + ' ' + removed_by})"
+        else:
+            removed_message = ""
+
+        email_lines.append(
+            f"{email_confirmation} {booking.owner_name[:20]:<20} {booking.court}: {start_time}-{end_time} {removed_message}"
+        )
+
+    email_body = "\n".join(email_lines)
+    msg = MIMEText(email_body)
+    msg["Subject"] = f"WNBA Bookings: {from_date.date()}"
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
+        smtp_server.login(FROM_ADDRESS, SMTP_PASSWORD)
+        smtp_server.sendmail(FROM_ADDRESS, TO_ADDRESSES, msg.as_string())
+
+
+def bookings_handler(event, context):
+    # Lambda function entrypoint
+    access_token = authenticate(HELLO_CLUB_USERNAME, HELLO_CLUB_PASSWORD)
+    client = HelloClubClient(access_token)
+
+    from_date = datetime.now(tz=PACIFIC_AUCKLAND)
+    to_date = from_date + timedelta(days=1) - timedelta(milliseconds=1)
+
+    email_bookings(client, from_date, to_date)
+
+
 if __name__ == "__main__":
+    # bookings_handler(None, None)
     parser = argparse.ArgumentParser(prog="bookings")
     parser.add_argument("--from-date", required=False)
     parser.add_argument("--removed", required=False, default=False, action="store_true")
@@ -139,9 +199,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    access_token = authenticate(
-        os.environ["HELLO_CLUB_USERNAME"], os.environ["HELLO_CLUB_PASSWORD"]
-    )
+    access_token = authenticate(HELLO_CLUB_USERNAME, HELLO_CLUB_PASSWORD)
     client = HelloClubClient(access_token)
 
     from_date = (
