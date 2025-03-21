9c9a183340771a89ac8414e9e5ea6aa10a986792
Luke Falvey <lukebfalvey@gmail.com>
2025-03-21 19:36:08 +1300

    Send a CSV file

--- src/bookings.py
+++ src/bookings.py
@@ -2,13 +2,14 @@ from datetime import datetime, timedelta
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
-from email.utils import COMMASPACE
 import smtplib
 import zoneinfo
 import argparse
 import os
 import copy
 import dataclasses
+import csv
+import tempfile
 
 import tabulate
 
@@ -151,41 +152,58 @@ def email_bookings(client: HelloClubClient, from_date: datetime, to_date: dateti
         Booking.from_dict(booking) for booking in removed_bookings if booking
     ]
 
-    booking_lines = []
-
-    for booking in sorted(bookings + removed_bookings):
-        email_confirmation = (
-            booking.confirmation_email_sent.strftime("%Y-%m-%d %H:%M:%S")
-            if booking.confirmation_email_sent
-            else ""
+    with tempfile.NamedTemporaryFile(mode="a+", encoding="utf-8-sig") as f:
+        writer = csv.DictWriter(
+            f,
+            fieldnames=[
+                "Email Confirmation Time",
+                "Booking Owner",
+                "Court",
+                "Booking Start",
+                "Booking End",
+                "Removed By",
+                "Removed At",
+            ],
         )
-        start_time = booking.start_time.strftime("%H:%M:%S")
-        end_time = booking.end_time.strftime("%H:%M:%S")
-        if booking.removed_on:
-            removed_on = booking.removed_on.strftime("%Y-%m-%d %H:%M:%S")
-            removed_by = booking.removed_by if booking.removed_by else "UNKNOWN"
-            removed_message = f"(REMOVED {removed_on + ' ' + removed_by})"
-        else:
-            removed_message = ""
+        writer.writeheader()
 
-        booking_lines.append(
-            f"{email_confirmation} {booking.owner_name[:20]:<20} {booking.court}: {start_time}-{end_time} {removed_message}"
-        )
+        for booking in sorted(bookings + removed_bookings):
+            writer.writerow(
+                {
+                    "Email Confirmation Time": booking.confirmation_email_sent.strftime(
+                        "%Y-%m-%d %H:%M:%S"
+                    )
+                    if booking.confirmation_email_sent
+                    else "",
+                    "Booking Owner": booking.owner_name,
+                    "Court": booking.court,
+                    "Booking Start": booking.start_time.strftime("%H:%M:%S"),
+                    "Booking End": booking.end_time.strftime("%H:%M:%S"),
+                    "Removed By": booking.removed_by
+                    if booking.removed_by
+                    else "UNKNOWN"
+                    if booking.removed_by
+                    else "",
+                    "Removed At": booking.removed_on.strftime("%Y-%m-%d %H:%M:%S")
+                    if booking.removed_on
+                    else "",
+                }
+            )
 
-    bookings_log = "\n".join(booking_lines)
+        f.seek(0)
 
-    msg = MIMEMultipart()
-    msg["From"] = FROM_ADDRESS
-    msg["To"] = TO_ADDRESSES
-    msg["Subject"] = f"WNBA Bookings: {from_date.date()}"
-    msg.attach(MIMEText(f"Please find attached bookings from: {from_date} - {to_date}"))
-    part = MIMEApplication(bookings_log, Name="bookings.log")
-    part["Content-Disposition"] = 'attachment; filename="bookings.log"'
-    msg.attach(part)
+        msg = MIMEMultipart()
+        msg["From"] = FROM_ADDRESS
+        msg["To"] = TO_ADDRESSES
+        msg["Subject"] = f"WNBA Bookings: {from_date.date()}"
+        msg.attach(MIMEText(f"Please find attached bookings from: {from_date} - {to_date}"))
+        part = MIMEApplication(f.read(), Name="bookings.csv")
+        part["Content-Disposition"] = 'attachment; filename="bookings.csv"'
+        msg.attach(part)
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
-        smtp_server.login(FROM_ADDRESS, SMTP_PASSWORD)
-        smtp_server.sendmail(FROM_ADDRESS, TO_ADDRESSES, msg.as_string())
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
+            smtp_server.login(FROM_ADDRESS, SMTP_PASSWORD)
+            smtp_server.sendmail(FROM_ADDRESS, TO_ADDRESSES, msg.as_string())
 
 
 def bookings_handler(event, context):
@@ -193,39 +211,41 @@ def bookings_handler(event, context):
     access_token = authenticate(HELLO_CLUB_USERNAME, HELLO_CLUB_PASSWORD)
     client = HelloClubClient(access_token)
 
-    from_date = datetime.now(tz=PACIFIC_AUCKLAND)
+    from_date = datetime.now(tz=PACIFIC_AUCKLAND).replace(
+        hour=0, minute=0, second=0, microsecond=0
+    )
     to_date = from_date + timedelta(days=1) - timedelta(milliseconds=1)
 
     email_bookings(client, from_date, to_date)
 
 
 if __name__ == "__main__":
-    # bookings_handler(None, None)
-    parser = argparse.ArgumentParser(prog="bookings")
-    parser.add_argument("--from-date", required=False)
-    parser.add_argument("--removed", required=False, default=False, action="store_true")
-    parser.add_argument(
-        "--timetable", required=False, default=False, action="store_true"
-    )
+    bookings_handler(None, None)
+    # parser = argparse.ArgumentParser(prog="bookings")
+    # parser.add_argument("--from-date", required=False)
+    # parser.add_argument("--removed", required=False, default=False, action="store_true")
+    # parser.add_argument(
+    #     "--timetable", required=False, default=False, action="store_true"
+    # )
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
-    access_token = authenticate(HELLO_CLUB_USERNAME, HELLO_CLUB_PASSWORD)
-    client = HelloClubClient(access_token)
+    # access_token = authenticate(HELLO_CLUB_USERNAME, HELLO_CLUB_PASSWORD)
+    # client = HelloClubClient(access_token)
 
-    from_date = (
-        datetime.fromisoformat(args.from_date).replace(tzinfo=PACIFIC_AUCKLAND)
-        if args.from_date
-        else datetime.today()
-        .replace(hour=0, minute=0, second=0, microsecond=0)
-        .astimezone(PACIFIC_AUCKLAND)
-    )
-    to_date = (from_date + timedelta(days=1) - timedelta(milliseconds=1)).astimezone(
-        PACIFIC_AUCKLAND
-    )
+    # from_date = (
+    #     datetime.fromisoformat(args.from_date).replace(tzinfo=PACIFIC_AUCKLAND)
+    #     if args.from_date
+    #     else datetime.today()
+    #     .replace(hour=0, minute=0, second=0, microsecond=0)
+    #     .astimezone(PACIFIC_AUCKLAND)
+    # )
+    # to_date = (from_date + timedelta(days=1) - timedelta(milliseconds=1)).astimezone(
+    #     PACIFIC_AUCKLAND
+    # )
 
-    print(from_date, to_date, "\n")
-    if args.timetable:
-        print_timetable(client, from_date, to_date)
-    else:
-        print_bookings(client, from_date, to_date)
+    # print(from_date, to_date, "\n")
+    # if args.timetable:
+    #     print_timetable(client, from_date, to_date)
+    # else:
+    #     print_bookings(client, from_date, to_date)
